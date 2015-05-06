c699639b9a7ca403d6a25e76966607a5fb4fb5e7
Klafyvel <klafyvel@gmail.com>
2015-05-06 11:50:06 +0200

    Ajout de la licence.

--- nom molecule.py
+++ nom molecule.py
@@ -1,4 +1,20 @@
-﻿# Créé par honnoraty, le 26/01/2015 en Python 3.2
+﻿# Nomenclature
+# Copyright (C) 2015 BOUVIN Valentin, HONNORATY Vincent, LEVY-FALK Hugo
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Créé par honnoraty, le 26/01/2015 en Python 3.2
 from molecule import *
 sourcechaine = dict()
 sourcechaine["methan"] = 1,4
